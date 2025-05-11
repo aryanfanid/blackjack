f48c2e35425b09a2281ff56c5c994a810897119d
aryanfanid <aryan.fanid90@yahoo.com>
2025-05-11 23:56:42 +0330

    [ADD] some behaviours and improvements

--- src/Game.java
+++ src/Game.java
@@ -1,28 +1,32 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.sql.SQLOutput;
+import java.util.*;
 
 public class Game {
     private List<Card> deck;
-
     private List<Card> playerHand;
     private List<Card> dealerHand;
 
     private Random random;
+    private Scanner scanner;
+    private boolean hideDealerCard = true;
 
     public Game() {
         deck = new ArrayList<>();
-
         playerHand = new ArrayList<>();
         dealerHand = new ArrayList<>();
 
         random = new Random();
+        scanner = new Scanner(System.in);
 
         for (Suit suit : Suit.values()) {
             for (Rank rank : Rank.values()) {
                 deck.add(new Card(suit, rank));
             }
         }
+    }
+
+    public void closeScanner() {
+        scanner.close();
     }
 
     public Card drawCard() {
@@ -38,20 +42,72 @@ public class Game {
     }
 
     public void printPlayerHand() {
+        System.out.println("========== PLAYER HAND ==========");
+        int total = 0;
         for (Card card : playerHand) {
+            total += card.getBlackjackValue();
             System.out.println(card);
         }
+        System.out.println("---------- TOTAL VALUE: " + total + " ----------\n");
     }
 
     public void printDealerHand() {
-        for (Card card : dealerHand) {
-            System.out.println(card);
+        System.out.println("========== DEALER HAND ==========");
+        int total = 0;
+        for (int i = 0; i < dealerHand.size(); i++) {
+
+            if (hideDealerCard && i == 1) {
+                System.out.println("***** of ***** * (Value: Unknown)");
+            } else {
+                Card card = dealerHand.get(i);
+                total += card.getBlackjackValue();
+                System.out.println(card);
+            }
         }
+        System.out.println("---------- TOTAL VALUE: " + total + " ----------\n");
     }
 
     public void printCards() {
         for (Card card : deck) {
             System.out.println(card);
         }
+    }
+
+    private boolean validateInput(String input) {
+        List<String> answers = List.of("h", "s");
+        return answers.contains(input);
+    }
+
+    private void showDealerCard() {
+        this.hideDealerCard = false;
+    }
+
+    private void hit() {
+        playerHand.add(drawCard());
+    }
+
+    private void stand() {
+    }
+
+    public void promptOperation() {
+        System.out.print("You wanna (H)it or (S)tand? ");
+
+        String input = scanner.nextLine();
+
+        boolean valid = this.validateInput(input);
+
+        if (valid) {
+            if (input.equals("h")) {
+                this.hit();
+            }
+            if (input.equals("s")) {
+                this.stand();
+            }
+        }
+        this.showDealerCard();
+    }
+
+    public void play() {
+        System.out.println("Playing...");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Game blackjack = new Game();
-//        blackjack.printCards();
 
         blackjack.dealCards();
-        blackjack.printPlayerHand();
-        blackjack.printDealerHand();
+        while (true) {
+            blackjack.printPlayerHand();
+            blackjack.printDealerHand();
 
+            blackjack.promptOperation();
+
+            blackjack.play();
+        }
+//        blackjack.closeScanner();
     }
 }
